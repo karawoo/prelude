4d3baee57293b7244bfbc3a589d2eb9c4554efdc
Kara Woo <woo.kara@gmail.com>
2025-04-07 10:34:48 -0700

    use native pipe

--- personal/custom.el
+++ personal/custom.el
@@ -254,13 +254,13 @@ sentence. Otherwise kill forward but preserve any punctuation at the sentence en
           '(lambda()
              (local-set-key [(s-return)] 'ess-use-this-dir)))
 
-;; %>% shortcut
+;; pipe shortcut
 ;; http://emacs.stackexchange.com/a/8055/7060
 (defun then_R_operator ()
   "R - %>% operator or 'then' pipe operator"
   (interactive)
   (just-one-space 1)
-  (insert "%>%")
+  (insert "|>")
   (reindent-then-newline-and-indent))
 (define-key ess-mode-map (kbd "C->") 'then_R_operator)
 (define-key inferior-ess-mode-map (kbd "C->") 'then_R_operator)
